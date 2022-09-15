ac3b8dfa119b2cff523dc5889eaeb4a0f0236e0d
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-09-15 18:41:05 -0500

    Added generic repository

---
+++ InvenTrac/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace InvenTrac.Repository.IRepository;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<T> GetOneAsync(Expression<Func<T, bool>> predicate);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    IEnumerable<T> GetSome(Expression<Func<T, bool>> predicate);
+    Task<bool> AddAsync(T entity);
+    bool Remove(T entity);
+    bool RemoveRange(IEnumerable<T> entity);
+    bool Update(T entity);
+}
+
---
+++ InvenTrac/Repository/Repository.cs
@@ -0,0 +1,68 @@
+﻿using InvenTrac.Data;
+using InvenTrac.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace InvenTrac.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+    internal DbSet<T> _dbSet;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task<bool> AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        return true;
+    }
+
+    public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AnyAsync(predicate);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<T> GetOneAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).FirstOrDefaultAsync();
+    }
+
+    public IEnumerable<T> GetSome(Expression<Func<T, bool>> predicate)
+    {
+        return _dbSet.Where(predicate);
+    }
+
+    public bool Remove(T entity)
+    {
+        _dbSet.Remove(entity);
+        return true;
+    }
+
+    public bool RemoveRange(IEnumerable<T> entity)
+    {
+        _dbSet.RemoveRange(entity);
+        return true;
+    }
+
+    public bool Update(T entity)
+    {
+        _context.Update(entity);
+        return true; // Remember to save changes to db after this method executes
+    }
+}
+
